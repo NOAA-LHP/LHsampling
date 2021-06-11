c55d82071ea3235ea70cca55a584e757fa1bee7d
Erin Bohaboy <65672694+ErinBohaboy-NOAA@users.noreply.github.com>
2021-06-11 12:20:27 -0400

    LH_plot function and documentation added

--- MUS_Functions_Code_POS_new.R
+++ MUS_Functions_Code_POS_new.R
@@ -4,7 +4,10 @@
 # 	eliminate variation in number of samples per length bin
 #
 #
-#  Latest update Mar30, 2021
+#  Latest update Jun10, 2021
+#	Amax and age_max arguments now optional in LH_sample
+#	LH_plot() updated
+#  update Mar30, 2021
 #	binl and propl have been changed to binL and propL to avoid ambiguous l letter vs. number
 
 
@@ -281,13 +284,12 @@ simulate_population_harvest <- function(Linf,Linf_sd, M, Lorenzen, F, mincat, ca
 #	  If user does not specify Amax or age_max arguments, they will be taken from the sim_output object
 #	  If user specifies Amax or age_max, the specified value will be used with a message and a reminder of the value
 #		used in the sim_output object to notify the user whether the values match.
-
 # -------------------------------------------------------------------------------
 
 #  -----------------  arguments, typical values
-#	sim_output <- pop_harv		# output of the simulate_population_harvest function
+#	sim_output <- S6_Auric_highF		# output of the simulate_population_harvest function
 #	n_boots <- 100			# number of bootstraps
-#   	samp_size <- 300			# sample size for each bootstrap
+#   	samp_size <- 305			# sample size for each bootstrap
 #	sample_type <- 'POS'		# define as 'POS' or 'FOS'
  
 #   	supp_large <- FALSE		# do we want to supplement with large fish?
@@ -299,10 +301,10 @@ simulate_population_harvest <- function(Linf,Linf_sd, M, Lorenzen, F, mincat, ca
 #   	constrained <- FALSE		# is t0 constrained?
 #   	t0 <- 0				# if constrainted, define t0
 
-#   	save_bootstraps <- TRUE		# should all bootstrap sample draws be saved? if n_boots or samp_size are large, this should be false
-#   	Amax <- 40				# maximum theoretical age (prop population input parameter)
-#   	age_max <- 20			# choose age_max (the plus group for pop dynamics) a priori
-#   	Lbin_width <- 5			# length bin width
+#   	save_bootstraps <- FALSE	# should all bootstrap sample draws be saved? if n_boots or samp_size are large, this should be false
+#   	Amax <- NULL			# maximum theoretical age (prop population input parameter)
+#   	age_max <- NULL			# choose age_max (the plus group for pop dynamics) a priori
+#   	Lbin_width <- 2			# length bin width
 
 # options(warn=0)
 
@@ -319,7 +321,7 @@ LH_sample <- function(sim_output, n_boots, samp_size, sample_type, supp_large = 
 
 #  define population_true and population_harvest
    	population_true <- sim_output$population	
-   	population_harv <- sim_output$harvest			
+   	population_harv <- sim_output$harvest	
 
 #  extract Amax and age_max from the sim_output
 #  if Amax and age_max are defined in LH_sample function arguments, use those but issue a message
@@ -590,7 +592,7 @@ for(i in 1:n_boots) {
 		POS <- POS_keep[,1:3]
           }
 	# at the conclusion of this do while loop, we should have correct samp_size
-		use_sample <- POS			#head(use_sample)		#str(use_sample)
+		use_sample <- POS			#head(use_sample)		#str(use_sample)	use_sample <- POS[,1:3]
     }	# ----------------------  END draw sample if POS
 
 
@@ -874,7 +876,7 @@ for(i in 1:n_boots) {
 	parameter_outputs[i,3] <- -1
 	}
 	
-	CV_age_boot <- list_boot_CVs[[i]]
+	CV_age_boot <- list_boot_CVs[[i]]			# CV_age_boot <- CV_boot
 	SD_age_boot <- list_boot_SDs[[i]]
 
 #  	 calculate CV_L_age0 and CV_L_ageMax
@@ -993,23 +995,31 @@ for(i in 1:n_boots) {
 
 
 
-
-
 # --------------------------------------------------------------------------------------------------------------------------------------
 # ------- PLOT FUNCTIONS TO SHOW RESULTS  -----------------------
 #	
 #
-#
+#	June 10: Adding some more plots and options
 #
 
-LH_plot <- function(sample_output, all_plots = TRUE) {
+
+#  -----------------  arguments, typical values
+#	sample_output <- S4_A2		# output of the LH_sample function
+#	output_type <- 'pdf'		# if = 'none' then plots are produced in the R graphics device only.
+#					 	# 	if = 'png' or 'pdf' then the corresponding file will also be produced and saved in the 
+#						#	current working directory
+
+
+#  ----- BEGIN FUNCTION ------------ 
+
+LH_plot <- function(sample_output, output_type = 'none') {
 
  look_at_me <- sample_output 
 
  dev.new()
 
-#  ---------- plot #: selectivity
- if(all_plots == TRUE) {
+#  ---------- plot #1: selectivity
+
   par(omi=c(0,0,0,0)) #set outer margins
   par(mai=c(1,0.8, 0.8, 0.4)) #set inner margins
 
@@ -1020,16 +1030,14 @@ LH_plot <- function(sample_output, all_plots = TRUE) {
   xmax = round((look_at_me$simulation_params$Linf + 0.20*look_at_me$simulation_params$Linf),0)
   plot(seq(0,xmax ,1),(pmin((1-pnorm(look_at_me$simulation_params$mincat,seq(0,xmax ,1),look_at_me$simulation_params$catsd)),
 				pnorm(look_at_me$simulation_params$maxcat,seq(0,xmax ,1),look_at_me$simulation_params$maxcatsd))),
-       type="l",xlab="Fish Length",ylab="Fishery Selectivity", main= main_title)
+       			type="l",xlab="Fish Length",ylab="Fishery Selectivity", main= main_title)
 	abline(v=look_at_me$simulation_params$mincat, lty=2, col="blue")
 	abline(v=look_at_me$simulation_params$maxcat, lty=2, col="blue")
 
-	readline(prompt="Press [enter] in R console for next figure")
- }
+	readline(prompt="Press [enter] in R console for next figure ")
+ 
+#  ---------- plot #2: natural mortality at age
 
-
-#  ---------- plot #: natural mortality at age
- if(all_plots == TRUE) {
   main_title <- paste("Natural Mortality at Age, Moverall =",
 				look_at_me$simulation_params$M, "Solve M1=", round(look_at_me$simulation_params$M1,2), 
 				sep=" ")
@@ -1041,11 +1049,10 @@ LH_plot <- function(sample_output, all_plots = TRUE) {
 	get(as.character(look_at_me$params_input_output$sim_output_name))$Avg_age$M_age,
 	xlab="Age", ylab="M at Age", main=main_title)
 
-  readline(prompt="Press [enter] in R console for next figure")
-}
+  readline(prompt="Press [enter] in R console for next figure ")
 
-#  ---------- plot #: pop age vs. length
- if(all_plots == TRUE) {
+#  ---------- plot #3: pop age vs. length (max N = 50,000)
+
   par(omi=c(0,0,0,0)) #set outer margins
   par(mai=c(1,0.8, 0.8, 0.4)) #set inner margins
 
@@ -1058,16 +1065,24 @@ LH_plot <- function(sample_output, all_plots = TRUE) {
   a0 = (log(1-(L0/Linf)))/k
   pred_lengths = Linf *(1-exp(-k*(input_plot_ages-a0)))
 
-  plot(get(as.character(look_at_me$params_input_output$sim_output_name))$population$age,
-	get(as.character(look_at_me$params_input_output$sim_output_name))$population$length,
+  sim_output_pop_age_length <- data.frame(age = get(as.character(look_at_me$params_input_output$sim_output_name))$population$age,
+							length = get(as.character(look_at_me$params_input_output$sim_output_name))$population$length)
+  if (nrow(sim_output_pop_age_length)>50000) {
+	plot_me <- sample_n(sim_output_pop_age_length, size = 50000,replace=FALSE)
+	  } else {
+	plot_me <- sim_output_pop_age_length
+	  }
+
+  plot(plot_me$age,
+	plot_me$length,
 	xlab="Age", ylab="Length", main="Population Age vs. Length w/ simulation input growth")
   lines(input_plot_ages, pred_lengths, lty=1, col="blue", lwd=2)
 
-  readline(prompt="Press [enter] in R console for next figure")
-}
+  readline(prompt="Press [enter] in R console for next figure ")
 
-# ---------- plot #: population prop at age and harvest histograms
- if(all_plots == TRUE) {
+
+# ---------- plot #4: population prop at age and harvest histograms
+
   age_breaks = seq(0,look_at_me$params_input_output$Amax,1)
   maxL_pop <- max(get(as.character(look_at_me$params_input_output$sim_output_name))$population$length)
   length_breaks = seq(0,maxL_pop+look_at_me$params_input_output$Lbin_width,look_at_me$params_input_output$Lbin_width)
@@ -1099,10 +1114,10 @@ LH_plot <- function(sample_output, all_plots = TRUE) {
 				"population and harvest histograms", sep=" ")
   mtext(side=3, main_title, line=0,outer=TRUE)
 
-  readline(prompt="Press [enter] in R console for next figure")
-}
+  readline(prompt="Press [enter] in R console for next figure ")
 
-#  ---------- plot #: A few bootstraps
+
+#  ---------- plot #5: A few bootstraps
 
   par(mfrow=c(3,3))
   par(omi=c(0.3,0.3,0.3,0.1)) #set outer margins
@@ -1123,7 +1138,231 @@ LH_plot <- function(sample_output, all_plots = TRUE) {
 	mtext(side=3, main_title, line=0,outer=TRUE)
 
 
+ #----------- if user wants to save outputs:
+
+ # --- write pdf outputs  
+   if(output_type == 'pdf') {
+    get_time <- Sys.time()
+    timestamp <- paste0(substr(get_time,1,4),substr(get_time,6,7),substr(get_time,9,10),
+		substr(get_time,12,13),substr(get_time,15,16),substr(get_time,18,19))
+    pdf(paste0("LH_plot_",timestamp,".pdf"))
+    # repeat all graphs
+	#  ---------- plot #1: selectivity
+  par(omi=c(0,0,0,0)) #set outer margins
+  par(mai=c(1,0.8, 0.8, 0.4)) #set inner margins
+  main_title <- paste(look_at_me$params_input_output$sim_output_name, "Selectivity at Length", "Min=",
+				look_at_me$simulation_params$mincat, "Max=",look_at_me$simulation_params$maxcat,
+				sep=" ")
+  xmax = round((look_at_me$simulation_params$Linf + 0.20*look_at_me$simulation_params$Linf),0)
+  plot(seq(0,xmax ,1),(pmin((1-pnorm(look_at_me$simulation_params$mincat,seq(0,xmax ,1),look_at_me$simulation_params$catsd)),
+				pnorm(look_at_me$simulation_params$maxcat,seq(0,xmax ,1),look_at_me$simulation_params$maxcatsd))),
+       type="l",xlab="Fish Length",ylab="Fishery Selectivity", main= main_title)
+	abline(v=look_at_me$simulation_params$mincat, lty=2, col="blue")
+	abline(v=look_at_me$simulation_params$maxcat, lty=2, col="blue")
+#  ---------- plot #2: natural mortality at age
+  main_title <- paste("Natural Mortality at Age, Moverall =",
+				look_at_me$simulation_params$M, "Solve M1=", round(look_at_me$simulation_params$M1,2), 
+				sep=" ")
+  par(omi=c(0,0,0,0)) #set outer margins
+  par(mai=c(1,0.8, 0.8, 0.4)) #set inner margins
+  plot(get(as.character(look_at_me$params_input_output$sim_output_name))$Avg_age$Ages,
+	get(as.character(look_at_me$params_input_output$sim_output_name))$Avg_age$M_age,
+	xlab="Age", ylab="M at Age", main=main_title)
+#  ---------- plot #3: pop age vs. length (max N = 50,000)
+  par(omi=c(0,0,0,0)) #set outer margins
+  par(mai=c(1,0.8, 0.8, 0.4)) #set inner margins
+  input_plot_ages <- seq(0,look_at_me$params_input_output$Amax,0.1)
+  Linf <- get(as.character(look_at_me$params_input_output$sim_output_name))$parameters$Linf
+  Linf_sd <- get(as.character(look_at_me$params_input_output$sim_output_name))$parameters$Linf_sd
+  L0 <- get(as.character(look_at_me$params_input_output$sim_output_name))$parameters$L0
+  L0_sd <- get(as.character(look_at_me$params_input_output$sim_output_name))$parameters$L0_sd
+  k <- get(as.character(look_at_me$params_input_output$sim_output_name))$parameters$k
+  a0 = (log(1-(L0/Linf)))/k
+  pred_lengths = Linf *(1-exp(-k*(input_plot_ages-a0)))
+  sim_output_pop_age_length <- data.frame(age = get(as.character(look_at_me$params_input_output$sim_output_name))$population$age,
+							length = get(as.character(look_at_me$params_input_output$sim_output_name))$population$length)
+  if (nrow(sim_output_pop_age_length)>50000) {
+	plot_me <- sample_n(sim_output_pop_age_length, size = 50000,replace=FALSE)
+	  } else {
+	plot_me <- sim_output_pop_age_length
+	  }
+  plot(plot_me$age,
+	plot_me$length,
+	xlab="Age", ylab="Length", main="Population Age vs. Length w/ simulation input growth")
+  lines(input_plot_ages, pred_lengths, lty=1, col="blue", lwd=2)
+ # ---------- plot #4: population prop at age and harvest histograms
+  age_breaks = seq(0,look_at_me$params_input_output$Amax,1)
+  maxL_pop <- max(get(as.character(look_at_me$params_input_output$sim_output_name))$population$length)
+  length_breaks = seq(0,maxL_pop+look_at_me$params_input_output$Lbin_width,look_at_me$params_input_output$Lbin_width)
+  par(mfcol=c(2,2))
+  par(omi=c(0.3,0.3,0.3,0.1)) #set outer margins
+  par(mai=c(0.4,0.4, 0.3, 0)) #set inner margins
+  hist(get(as.character(look_at_me$params_input_output$sim_output_name))$population$length,
+	breaks=length_breaks, include.lowest=TRUE, right=FALSE,plot=TRUE,
+	main="Population Lengths",xlab="Length",ylab="Frequency")
+  hist(get(as.character(look_at_me$params_input_output$sim_output_name))$harvest$length,
+	breaks=length_breaks, include.lowest=TRUE, right=FALSE,plot=TRUE,
+	main="Harvest Lengths",xlab="Length",ylab="Frequency")
+  mtext(side=1, "Length", line=2.5,outer=FALSE)
+  hist(get(as.character(look_at_me$params_input_output$sim_output_name))$population$age,
+	breaks=age_breaks, include.lowest=TRUE, right=FALSE,plot=TRUE,
+	main="Population Ages",xlab="Length",ylab="Frequency")
+  hist(get(as.character(look_at_me$params_input_output$sim_output_name))$harvest$age,
+	breaks=age_breaks, include.lowest=TRUE, right=FALSE,plot=TRUE,
+	main="Harvest Ages",xlab="Length",ylab="Frequency")
+  mtext(side=1, "Age", line=2.5,outer=FALSE)
+  mtext(side=2, "Frequency",line=0,outer=TRUE)
+  main_title <- paste(look_at_me$params_input_output$sim_output_name,
+				"population and harvest histograms", sep=" ")
+  mtext(side=3, main_title, line=0,outer=TRUE)
+#  ---------- plot #5: A few bootstraps
+  par(mfrow=c(3,3))
+  par(omi=c(0.3,0.3,0.3,0.1)) #set outer margins
+  par(mai=c(0.2,0.2, 0.3, 0)) #set inner margins
+  for (j in 1:9) {
+	sample_size <- sum(look_at_me$list_some_boot_samps[[j]]$counts)
+	title_temp <- paste("samp boot",j,"n=",sample_size,sep=" ")
+	plot(look_at_me$list_some_boot_samps[[j]], main = title_temp)
+   }
+	mtext(side=1,"Length",line=1,outer=TRUE)
+	mtext(side=2, "Frequency",line=1,outer=TRUE)
+	main_title <- paste(look_at_me$params_input_output$sim_output_name,
+				look_at_me$params_input_output$sample_type,
+				"supp sm", look_at_me$params_input_output$supp_small,
+				"supp lg", look_at_me$params_input_output$supp_large,
+				":   A Few Bootstraps", sep=" ")
+	mtext(side=3, main_title, line=0,outer=TRUE)
+	
+	dev.off()
+  }
+ 
+ # --- write png outputs  
+   if(output_type == 'png') {
+    get_time <- Sys.time()
+    timestamp <- paste0(substr(get_time,1,4),substr(get_time,6,7),substr(get_time,9,10),
+		substr(get_time,12,13),substr(get_time,15,16),substr(get_time,18,19))
+   
+    # repeat all graphs, define seperate png before each
+    
+ #  ---------- plot #1: selectivity
+  png(paste0("1_selex_",timestamp,".png"))
+  par(omi=c(0,0,0,0)) #set outer margins
+  par(mai=c(1,0.8, 0.8, 0.4)) #set inner margins
+
+  main_title <- paste(look_at_me$params_input_output$sim_output_name, "Selectivity at Length", "Min=",
+				look_at_me$simulation_params$mincat, "Max=",look_at_me$simulation_params$maxcat,
+				sep=" ")
+
+  xmax = round((look_at_me$simulation_params$Linf + 0.20*look_at_me$simulation_params$Linf),0)
+  plot(seq(0,xmax ,1),(pmin((1-pnorm(look_at_me$simulation_params$mincat,seq(0,xmax ,1),look_at_me$simulation_params$catsd)),
+				pnorm(look_at_me$simulation_params$maxcat,seq(0,xmax ,1),look_at_me$simulation_params$maxcatsd))),
+       type="l",xlab="Fish Length",ylab="Fishery Selectivity", main= main_title)
+	abline(v=look_at_me$simulation_params$mincat, lty=2, col="blue")
+	abline(v=look_at_me$simulation_params$maxcat, lty=2, col="blue")
+  dev.off()
+
+#  ---------- plot #2: natural mortality at age
+  png(paste0("2_MatAge_",timestamp,".png"))
+  main_title <- paste("Natural Mortality at Age, Moverall =",
+				look_at_me$simulation_params$M, "Solve M1=", round(look_at_me$simulation_params$M1,2), 
+				sep=" ")
+
+  par(omi=c(0,0,0,0)) #set outer margins
+  par(mai=c(1,0.8, 0.8, 0.4)) #set inner margins
+
+  plot(get(as.character(look_at_me$params_input_output$sim_output_name))$Avg_age$Ages,
+	get(as.character(look_at_me$params_input_output$sim_output_name))$Avg_age$M_age,
+	xlab="Age", ylab="M at Age", main=main_title)
+  dev.off()
+
+#  ---------- plot #3: pop age vs. length (max N = 50,000)
+  png(paste0("3_Pop_AgeLength_",timestamp,".png"))
+  par(omi=c(0,0,0,0)) #set outer margins
+  par(mai=c(1,0.8, 0.8, 0.4)) #set inner margins
+
+  input_plot_ages <- seq(0,look_at_me$params_input_output$Amax,0.1)
+  Linf <- get(as.character(look_at_me$params_input_output$sim_output_name))$parameters$Linf
+  Linf_sd <- get(as.character(look_at_me$params_input_output$sim_output_name))$parameters$Linf_sd
+  L0 <- get(as.character(look_at_me$params_input_output$sim_output_name))$parameters$L0
+  L0_sd <- get(as.character(look_at_me$params_input_output$sim_output_name))$parameters$L0_sd
+  k <- get(as.character(look_at_me$params_input_output$sim_output_name))$parameters$k
+  a0 = (log(1-(L0/Linf)))/k
+  pred_lengths = Linf *(1-exp(-k*(input_plot_ages-a0)))
+
+  sim_output_pop_age_length <- data.frame(age = get(as.character(look_at_me$params_input_output$sim_output_name))$population$age,
+							length = get(as.character(look_at_me$params_input_output$sim_output_name))$population$length)
+  if (nrow(sim_output_pop_age_length)>50000) {
+	plot_me <- sample_n(sim_output_pop_age_length, size = 50000,replace=FALSE)
+	  } else {
+	plot_me <- sim_output_pop_age_length
+	  }
+
+  plot(plot_me$age,
+	plot_me$length,
+	xlab="Age", ylab="Length", main="Population Age vs. Length w/ simulation input growth")
+  lines(input_plot_ages, pred_lengths, lty=1, col="blue", lwd=2)
+  dev.off()
+
+# ---------- plot #4: population prop at age and harvest histograms
+  png(paste0("4_Pop_Harv_Histograms_",timestamp,".png"))
+  age_breaks = seq(0,look_at_me$params_input_output$Amax,1)
+  maxL_pop <- max(get(as.character(look_at_me$params_input_output$sim_output_name))$population$length)
+  length_breaks = seq(0,maxL_pop+look_at_me$params_input_output$Lbin_width,look_at_me$params_input_output$Lbin_width)
+
+  par(mfcol=c(2,2))
+  par(omi=c(0.3,0.3,0.3,0.1)) #set outer margins
+  par(mai=c(0.4,0.4, 0.3, 0)) #set inner margins
+
+  hist(get(as.character(look_at_me$params_input_output$sim_output_name))$population$length,
+	breaks=length_breaks, include.lowest=TRUE, right=FALSE,plot=TRUE,
+	main="Population Lengths",xlab="Length",ylab="Frequency")
+
+  hist(get(as.character(look_at_me$params_input_output$sim_output_name))$harvest$length,
+	breaks=length_breaks, include.lowest=TRUE, right=FALSE,plot=TRUE,
+	main="Harvest Lengths",xlab="Length",ylab="Frequency")
+  mtext(side=1, "Length", line=2.5,outer=FALSE)
+
+  hist(get(as.character(look_at_me$params_input_output$sim_output_name))$population$age,
+	breaks=age_breaks, include.lowest=TRUE, right=FALSE,plot=TRUE,
+	main="Population Ages",xlab="Length",ylab="Frequency")
+
+  hist(get(as.character(look_at_me$params_input_output$sim_output_name))$harvest$age,
+	breaks=age_breaks, include.lowest=TRUE, right=FALSE,plot=TRUE,
+	main="Harvest Ages",xlab="Length",ylab="Frequency")
+  mtext(side=1, "Age", line=2.5,outer=FALSE)
+
+  mtext(side=2, "Frequency",line=0,outer=TRUE)
+  main_title <- paste(look_at_me$params_input_output$sim_output_name,
+				"population and harvest histograms", sep=" ")
+  mtext(side=3, main_title, line=0,outer=TRUE)
+  dev.off()
+
+#  ---------- plot #5: A few bootstraps
+  png(paste0("5_A_Few_Bootstraps_",timestamp,".png"))
+  par(mfrow=c(3,3))
+  par(omi=c(0.3,0.3,0.3,0.1)) #set outer margins
+  par(mai=c(0.2,0.2, 0.3, 0)) #set inner margins
+
+  for (j in 1:9) {
+	sample_size <- sum(look_at_me$list_some_boot_samps[[j]]$counts)
+	title_temp <- paste("samp boot",j,"n=",sample_size,sep=" ")
+	plot(look_at_me$list_some_boot_samps[[j]], main = title_temp)
+   }
+	mtext(side=1,"Length",line=1,outer=TRUE)
+	mtext(side=2, "Frequency",line=1,outer=TRUE)
+	main_title <- paste(look_at_me$params_input_output$sim_output_name,
+				look_at_me$params_input_output$sample_type,
+				"supp sm", look_at_me$params_input_output$supp_small,
+				"supp lg", look_at_me$params_input_output$supp_large,
+				":   A Few Bootstraps", sep=" ")
+	mtext(side=3, main_title, line=0,outer=TRUE)
+  dev.off()
+
+ 	}
+
+
   }	#----------- end plot function
+
 
 
 # make a function workspace
